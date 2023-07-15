6ecda07a5c0478d20512d907a74b9a20849ce7fd
Shayth <Shaythyce@gmail.com>
2023-07-15 21:31:16 +0300

    debug

--- bot.py
+++ bot.py
@@ -49,15 +49,6 @@ def get_another_info():
                       InlineKeyboardButton("Не выполнено", callback_data="cb_no"))
     butmsg = bot.send_message(ready_id, f'{ready_test_table}\nВремя на ответ: {ready_answer_time} секунд',
                      reply_markup=markup_inline)
-    print('Кнопки вызваны')
-    start_time = 0
-    while start_time < int(ready_answer_time):
-        start_time = start_time + 1
-        time.sleep(1)
-    else:
-        bot.edit_message_text("Время истекло", chat_id=ready_id, message_id=butmsg.message_id)
-        bot.send_message(admin_id, "[Админ, время выполнения задания истекло]")
-
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
@@ -66,11 +57,13 @@ def callback_query(call):
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Выполнено",
                               reply_markup=None)
         bot.send_message(admin_id, "[Админ, задание выполнено]")
+        check_task()
     if call.data == "cb_no":
         bot.answer_callback_query(call.id, "")
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Не Выполнено",
                               reply_markup=None)
         bot.send_message(admin_id, "[Админ, задание не выполнено]")
+        check_task()
 
 
 if __name__ == '__main__':
